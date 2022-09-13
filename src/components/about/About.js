39a2cbda577ffc445fc49599793ed79ddc7b7272
Dalvin Segura <byjacson18@gmail.com>
2022-09-12 21:34:26 -0400

    Portfolio section created missing grid layout for each project added also missed add some margin to project-container

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,14 +11,28 @@ export default function About() {
         <div className="about-container">
           <div className="text-content-about">
             <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam exercitationem tempore in quibusdam natus officia quas aperiam corporis error perspiciatis mollitia nemo porro ex, amet ratione recusandae facilis distinctio cupiditate.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae magnam laborum deleniti.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+              exercitationem tempore in quibusdam natus officia quas aperiam
+              corporis error perspiciatis mollitia nemo porro ex, amet ratione
+              recusandae facilis distinctio cupiditate.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Recusandae magnam laborum deleniti.
+            </p>
           </div>
-    
-      <div className="programmer-illustration-content">
-        <img className="floating" src={require("../../images/about/programmer-illustration.svg").default} alt="Programmer Illustration" />
-      </div>
 
+          <div className="programmer-illustration-content">
+            <img
+              className="floating"
+              src={
+                require("../../images/about/programmer-illustration.svg")
+                  .default
+              }
+              alt="Programmer Illustration"
+            />
+          </div>
         </div>
       </div>
       <ParticleBackground />
--- src/components/portafolio/Portfolio.js
+++ src/components/portafolio/Portfolio.js
@@ -1,13 +1,103 @@
 import NavBar from "../navbar/NavBar";
 import ParticleBackground from "../ParticleBackground";
+import "./portfolio.scss";
 
+// IMPORT AN IMAGE FOR EACH PROJECT HERE:
+import macdonaldsPreviewImg from "../../images/portfolio/macdonalds-website.png";
+
+const ProjectCard = (props) => {
+  return (
+    <div className="project-card">
+      <img
+        id="img-project"
+        src={props.urlImage}
+        alt="Preview of the project"
+        onMouseOver={() => {
+          document
+            .getElementById("img-project")
+            .classList.add("zoom-img-hover");
+        }}
+        onMouseOut={() => {
+          document
+            .getElementById("img-project")
+            .classList.remove("zoom-img-hover");
+        }}
+      />
+      <h3>{props.title}</h3>
+
+      <div className="tech-used-content">
+        <p>{props.techUsed}</p>
+      </div>
+
+      <div className="btn-container">
+        <a
+          href={props.codeGithubUrl}
+          target="_blank"
+          alt="Link to project code"
+        >
+          <i class="fa-brands fa-github github-icon"></i>
+          <span className="text-github">Code</span>
+        </a>
+        <a href={props.previewbUrl} target="_blank">
+          <i class="fa-solid fa-eye preview-icon"></i>
+          <span className="text-preview">Preview</span>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default function Portfolio() {
   return (
     <>
+      <div className="portfolio">
+        <NavBar />
+        <div className="portfolio-container">
+          <h1>Portfolio</h1>
+          <div className="projects-container">
+            <ProjectCard
+              urlImage={macdonaldsPreviewImg}
+              title="MacDonald's Website"
+              techUsed="ReactJS | SASS | Tsparticles | NodeJS | ExpressJS | SQL Server"
+              codeGithubUrl="https://github.com/dalvinsegura/portfolio"
+              previewbUrl="https://github.com/dalvinsegura"
+            />
+
+<ProjectCard
+              urlImage={macdonaldsPreviewImg}
+              title="MacDonald's Website"
+              techUsed="ReactJS | SASS | Tsparticles | NodeJS | ExpressJS | SQL Server"
+              codeGithubUrl="https://github.com/dalvinsegura/portfolio"
+              previewbUrl="https://github.com/dalvinsegura"
+            />
+
+<ProjectCard
+              urlImage={macdonaldsPreviewImg}
+              title="MacDonald's Website"
+              techUsed="ReactJS | SASS | Tsparticles | NodeJS | ExpressJS | SQL Server"
+              codeGithubUrl="https://github.com/dalvinsegura/portfolio"
+              previewbUrl="https://github.com/dalvinsegura"
+            />
+
+<ProjectCard
+              urlImage={macdonaldsPreviewImg}
+              title="MacDonald's Website"
+              techUsed="ReactJS | SASS | Tsparticles | NodeJS | ExpressJS | SQL Server"
+              codeGithubUrl="https://github.com/dalvinsegura/portfolio"
+              previewbUrl="https://github.com/dalvinsegura"
+            />
+
+<ProjectCard
+              urlImage={macdonaldsPreviewImg}
+              title="MacDonald's Website"
+              techUsed="ReactJS | SASS | Tsparticles | NodeJS | ExpressJS | SQL Server"
+              codeGithubUrl="https://github.com/dalvinsegura/portfolio"
+              previewbUrl="https://github.com/dalvinsegura"
+            />
+          </div>
+        </div>
+      </div>
       <ParticleBackground />
-      <NavBar />
-      <h1>Portfolio</h1>
     </>
   );
 }
