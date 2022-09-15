5687f236539b9c39659978ae5d59a5b7edfb83e4
Dalvin Segura <byjacson18@gmail.com>
2022-09-14 21:17:09 -0400

    - More icon were added for skill section.  - Skill icon container was improved with grid layout. - A components for showing up icons was created.

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -3,6 +3,39 @@ import ParticleBackground from "../ParticleBackground";
 
 import "./skills.scss";
 
+import FigmaIcon from "../../images/skills/figma.svg";
+import IllustratorIcon from "../../images/skills/adobe-illustrator.svg";
+import PhotoshopIcon from "../../images/skills/adobe-photoshop.svg";
+import CssIcon from "../../images/skills/css3.svg";
+import ExpressIcon from "../../images/skills/express.svg";
+import GitIcon from "../../images/skills/git.svg";
+import HtmlIcon from "../../images/skills/html.svg";
+import JavaScriptIcon from "../../images/skills/javascript.svg";
+import NodejsIcon from "../../images/skills/nodejs.svg";
+import NpmIcon from "../../images/skills/npm.svg";
+import ReactjsIcon from "../../images/skills/react.svg";
+import SassIcon from "../../images/skills/sass.svg";
+import SqlIcon from "../../images/skills/sql.svg";
+
+const IconSkill = (props) => {
+  return (
+    <img
+      onMouseOver={() => {
+        const skillIcon = document.getElementById("skills-icon");
+
+        skillIcon.classList.add("skills-icon-hover");
+        setTimeout(() => {
+          skillIcon.classList.remove("skills-icon-hover");
+        }, 1500);
+      }}
+      id="skills-icon"
+      className="skills-icon"
+      src={props.srcUrl}
+      alt={props.alt}
+    />
+  );
+};
+
 export default function Skills() {
   return (
     <div className="skills">
@@ -11,10 +44,13 @@ export default function Skills() {
         <div className="text-content-skills">
           <h1>Skills</h1>
           <p>
-          I am currently training as a full stack developer, but foremost my goal is to master ReactJS well.
+            I am currently training as a full stack developer, but foremost my
+            goal is to master ReactJS well.
           </p>
           <p>
-          I also could not forget to improve my other skills like the ones I mention here, which is why I have the goal of always keeping practicing them.
+            I also could not forget to improve my other skills like the ones I
+            mention here, which is why I have the goal of always keeping
+            practicing them.
           </p>
         </div>
 
@@ -29,55 +65,19 @@ export default function Skills() {
           id="skills-icon-content"
           className="skills-icon-content"
         >
-          <img
-            onMouseOver={() => {
-              const skillIcon = document.getElementById("skills-icon");
-
-              skillIcon.classList.add("skills-icon-hover");
-              setTimeout(() => {
-                skillIcon.classList.remove("skills-icon-hover");
-              }, 500);
-            }}
-            id="skills-icon"
-            className="skills-icon"
-            src={require("../../images/skills/react.svg").default}
-            alt="figma icon"
-          />
-          <img
-            className="skills-icon"
-            src={require("../../images/skills/nodejs.svg").default}
-            alt="figma icon"
-          />
-          <img
-            className="skills-icon"
-            src={require("../../images/skills/express.svg").default}
-            alt="figma icon"
-          />
-          <img
-            className="skills-icon"
-            src={require("../../images/skills/sql.svg").default}
-            alt="figma icon"
-          />
-          <img
-            className="skills-icon"
-            src={require("../../images/skills/npm.svg").default}
-            alt="figma icon"
-          />
-          <img
-            className="skills-icon"
-            src={require("../../images/skills/sass.svg").default}
-            alt="figma icon"
-          />
-          <img
-            className="skills-icon"
-            src={require("../../images/skills/figma.svg").default}
-            alt="figma icon"
-          />
-          <img
-            className="skills-icon"
-            src={require("../../images/skills/git.svg").default}
-            alt="figma icon"
-          />
+          <IconSkill srcUrl={HtmlIcon} alt="HTML Icon" />
+          <IconSkill srcUrl={CssIcon} alt="CSS Icon" />
+          <IconSkill srcUrl={SassIcon} alt="SASS Icon" />
+          <IconSkill srcUrl={FigmaIcon} alt="Figma Icon" />
+          <IconSkill srcUrl={PhotoshopIcon} alt="Photoshop Icon" />
+          <IconSkill srcUrl={IllustratorIcon} alt="Illustrator Icon" />
+          <IconSkill srcUrl={GitIcon} alt="Git Icon" />
+          <IconSkill srcUrl={JavaScriptIcon} alt="JavaScript Icon" />
+          <IconSkill srcUrl={NodejsIcon} alt="NodeJS Icon" />
+          <IconSkill srcUrl={SqlIcon} alt="SQL Server Icon" />
+          <IconSkill srcUrl={ReactjsIcon} alt="ReactJS Icon" />
+          <IconSkill srcUrl={ExpressIcon} alt="ExpressJS Icon" />
+          <IconSkill srcUrl={NpmIcon} alt="NPM Icon" />
         </div>
       </div>
       <ParticleBackground />
